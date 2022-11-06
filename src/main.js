6d8069987ece025835559b8d31781d2d9aa1b2cf
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2022-11-06 10:41:09 -0300

    main: Change reference main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,18 @@
-import { createApp } from 'vue';
+import { createApp } from './vue';
 import App from './App.vue';
 import router from './router';
 import './assets/css/app.css';
-import BackToTop from 'vue-backtotop';
+
 
 const feather = ('feather-icons');
 feather.replace();
 
 createApp(App)
 	.use(router)
-	.use(BackToTop)
 	.mount('#app');
 
 const appTheme = localStorage.getItem('theme');
 
-// Check what is the active theme and change theme when user clicks on the theme button in header.
 if (
 	appTheme === 'dark' &&
 	document.querySelector('body').classList.contains('app-theme')
