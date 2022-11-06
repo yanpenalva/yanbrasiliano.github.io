de9372676e86d0054968271cd34581baad1ad9d9
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2022-11-06 10:44:55 -0300

    main: Change reference main.js

--- src/main.js
+++ src/main.js
@@ -1,23 +1,20 @@
-import { createApp } from './vue';
-import App from './App.vue';
-import router from './router';
-import './assets/css/app.css';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/css/app.css";
 
-
-const feather = ('feather-icons');
+const feather = "feather-icons";
 feather.replace();
 
-createApp(App)
-	.use(router)
-	.mount('#app');
+createApp(App).use(router).mount("#app");
 
-const appTheme = localStorage.getItem('theme');
+const appTheme = localStorage.getItem("theme");
 
 if (
-	appTheme === 'dark' &&
-	document.querySelector('body').classList.contains('app-theme')
+  appTheme === "dark" &&
+  document.querySelector("body").classList.contains("app-theme")
 ) {
-	document.querySelector('body').classList.add('bg-primary-dark');
+  document.querySelector("body").classList.add("bg-primary-dark");
 } else {
-	document.querySelector('body').classList.add('bg-secondary-light');
+  document.querySelector("body").classList.add("bg-secondary-light");
 }
