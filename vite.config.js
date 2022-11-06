8395e2a2ad99d65e2f6eea7b8d2f3b24fe5ea7b1
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2022-11-06 11:59:43 -0300

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 const path = require("path");
 export default defineConfig({
+  base: "/yanbrasiliano.github.io/",
   plugins: [vue()],
   resolve: {
     alias: [
