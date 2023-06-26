a4357513be95f9aa353d8f1c8daea2172ac17596
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2023-06-26 20:29:07 -0300

    main: add version.

--- src/components/shared/FooterCopyright.vue
+++ src/components/shared/FooterCopyright.vue
@@ -1,28 +1,28 @@
 <script>
 export default {
-	data: () => {
-		return {
-			copyrightDate: new Date().getFullYear(),
-			author: 'Yan Penalva',
-		};
-	},
+  data: () => {
+    return {
+      copyrightDate: new Date().getFullYear(),
+      author: 'Yan Penalva',
+    };
+  },
 };
 </script>
 
 <template>
-	<div class="flex justify-center items-center text-center">
-		<div class="font-general-regular text-lg text-ternary-dark dark:text-ternary-light">
-			&copy; {{ copyrightDate }}
-
-			- Developed by
-			<a href="https://www.linkedin.com/in/yanbrasiliano/" target="__blank"
-				class="font-general-medium text-secondary-dark dark:text-secondary-light uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500">{{
-						author
-				}}</a>
-		</div>
-	</div>
+  <div class="flex justify-center items-center text-center">
+    <div class="font-general-regular text-lg text-ternary-dark dark:text-ternary-light">
+      &copy; {{ copyrightDate }}
+      <span>
+        - Developed by
+      </span>
+      <a href="https://www.linkedin.com/in/yanbrasiliano/" target="__blank"
+        class="font-general-medium text-secondary-dark dark:text-secondary-light uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 duration-500">{{
+          author
+        }}</a>
+    </div>
+  </div>
+  <span>v1.0.0</span>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
