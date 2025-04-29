d06004bfa2304120f0ee20e700f99a05d8f54f45
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2025-04-28 21:06:31 -0300

    chore: adjust resumes

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -1,19 +1,19 @@
 <script>
-import feather from 'feather-icons';
+import feather from "feather-icons";
 
 export default {
-    name: 'Home',
+    name: "Home",
     data: () => {
         return {
-            theme: '',
+            theme: "",
         };
     },
     created() {
-        this.theme = localStorage.getItem('theme') || 'light';
+        this.theme = localStorage.getItem("theme") || "light";
     },
     mounted() {
         feather.replace();
-        this.theme = localStorage.getItem('theme') || 'light';
+        this.theme = localStorage.getItem("theme") || "light";
     },
     updated() {
         feather.replace();
@@ -27,25 +27,33 @@ export default {
         <!-- Banner left contents -->
         <div class="w-full md:w-1/3 text-left">
             <h1
-                class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase whitespace-no-wrap">
+                class="font-general-semibold text-3xl md:text-3xl xl:text-4xl text-center sm:text-left text-ternary-dark dark:text-primary-light uppercase whitespace-no-wrap"
+            >
                 Yan Penalva
             </h1>
             <p
-                class="font-general-medium mt-2 text-lg sm:text-xl  xl:text-2xl text-center sm:text-left leading-none text-gray-400">
+                class="font-general-medium mt-2 text-lg sm:text-xl xl:text-2xl text-center sm:text-left leading-none text-gray-400"
+            >
                 Software Engineer
             </p>
             <div class="flex justify-center sm:block">
-                <a download="Resume_Portuguese_Yan.pdf" href="/files/Resume_Portuguese_Yan.pdf"
+                <a
+                    download="Yan_Penalva_Curriculo_PTBR.pdf"
+                    href="/files/Yan_Penalva_Curriculo_PTBR.pdf"
                     class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover-bg-indigo-500 text-gray-500 hover-text-white duration-500"
-                    aria-label="Download Resume">
+                    aria-label="Download Resume"
+                >
                     <i data-feather="arrow-down-circle" class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6 duration-100"></i>
                     <span class="text-sm sm:text-lg font-general-medium duration-100">Resume(PT-BR)</span>
                 </a>
             </div>
             <div class="flex justify-center sm:block">
-                <a download="Resume_English_Yan" href="/files/Resume_English_Yan.pdf"
+                <a
+                    download="Yan_Penalva_Resume_EN"
+                    href="/files/Yan_Penalva_Resume_EN.pdf"
                     class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover-bg-indigo-500 text-gray-500 hover-text-white duration-500"
-                    aria-label="Download Resume">
+                    aria-label="Download Resume"
+                >
                     <i data-feather="arrow-down-circle" class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6 duration-100"></i>
                     <span class="text-sm sm:text-lg font-general-medium duration-100">Resume(EN-US)</span>
                 </a>
@@ -59,6 +67,5 @@ export default {
         </div>
     </section>
 </template>
-
 
 <style scoped></style>
