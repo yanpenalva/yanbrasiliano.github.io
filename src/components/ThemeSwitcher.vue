22df84e876dac84fc8a6d79542496fd0a5c69422
Yan Brasiliano Silva Penalva <yanpenabr@gmail.com>
2025-08-10 21:11:39 -0300

    refactor: simplify theme switcher logic and improve computed properties

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -1,11 +1,23 @@
 <script>
 export default {
-    props: {
-        theme: { type: String, required: true },
+    props: { theme: { type: String, required: true } },
+    emits: ["themeChanged"],
+    computed: {
+        isLight() {
+            return this.theme === "light";
+        },
+        icon() {
+            return this.isLight ? "moon" : "sun";
+        },
+        iconClass() {
+            return this.isLight
+                ? "text-liText-ternary-dark hover:text-gray-400 dark:text-liText-ternary-light dark:hover:text-liBorder-primary-light w-5"
+                : "text-gray-200 hover:text-gray-50 w-5";
+        },
     },
     methods: {
         toggleTheme() {
-            const newTheme = this.theme === "light" ? "dark" : "light";
+            const newTheme = this.isLight ? "dark" : "light";
             localStorage.setItem("theme", newTheme);
             this.$emit("themeChanged", newTheme);
             location.reload();
@@ -16,13 +28,6 @@ export default {
 
 <template>
     <a href="#" @click.prevent="toggleTheme" aria-label="Theme Switcher">
-        <i
-            :data-feather="theme === 'light' ? 'moon' : 'sun'"
-            :class="
-                theme === 'light'
-                    ? 'text-liText-ternary-dark hover:text-gray-400 dark:text-liText-ternary-light dark:hover:text-liBorder-primary-light w-5'
-                    : 'text-gray-200 hover:text-gray-50 w-5'
-            "
-        ></i>
+        <i :data-feather="icon" :class="iconClass"></i>
     </a>
 </template>
